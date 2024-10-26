a1f5166b336f3658e7f35fc4e588c8eea050665b
Taylor <admin@vulehuan.com>
2024-10-26 19:37:40 +0700

    release initial version of bmi_calculator gem with BMI calculation and categorization features

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+## [0.1.0] - 2024-10-26
+### Added
+- Initial release of the `bmi_calculator` gem.
+- Added `calculate` method to compute BMI based on weight and height.
+- Added `bmi_category` method to categorize BMI into "Underweight", "Normal", "Overweight", and "Obesity".
--- lib/bmi_calculator.rb
+++ lib/bmi_calculator.rb
@@ -4,5 +4,22 @@ require_relative "bmi_calculator/version"
 
 module BmiCalculator
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.calculate(weight, height)
+    if height <= 0 || weight <= 0
+      raise ArgumentError, "Weight and height must be greater than 0"
+    end
+
+    bmi = weight / (height ** 2)
+    bmi.round(2)
+  end
+
+  def self.bmi_category(bmi)
+    case bmi
+    when 0..18.4 then "Underweight"
+    when 18.5..24.9 then "Normal"
+    when 25..29.9 then "Overweight"
+    else "Obesity"
+    end
+  end
 end
