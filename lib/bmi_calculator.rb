422381c3e84b8a10cb2e1574693a84ec6401b062
Taylor <admin@vulehuan.com>
2024-10-26 18:43:48 +0700

    Add auto-generated files after running 'bundle gem bmi_calculator'

---
+++ lib/bmi_calculator.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "bmi_calculator/version"
+
+module BmiCalculator
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/bmi_calculator/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module BmiCalculator
+  VERSION = "0.1.0"
+end
